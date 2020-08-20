5597cf250612327d4290417927d378a263daca37
Arnold KOUAKEP <kouakeparnold27@gmail.com>
2020-08-20 17:45:50 +0100

    Mise a jour du module DAO

--- bills-dao/src/main/java/cm/beni/main/julia/dao/controller/repository/master/NavireRepository.java
+++ bills-dao/src/main/java/cm/beni/main/julia/dao/controller/repository/master/NavireRepository.java
@@ -1,7 +1,5 @@
 package cm.beni.main.julia.dao.controller.repository.master;
 
-import java.util.Collection;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
